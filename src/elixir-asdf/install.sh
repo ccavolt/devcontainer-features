6f978c42b9f08b3adb386f5023f7b951ce5509db
Corbin Cavolt <ccavolt@pm.me>
2023-04-20 04:20:18 +0000

    Safety commit (Still not working) * Added more testing * Fixed install script so container actually finishes building now

--- src/elixir-asdf/install.sh
+++ src/elixir-asdf/install.sh
@@ -2,55 +2,66 @@
 
 set -e
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+# export LC_ALL=en_US.UTF-8
+# export LANG=en_US.UTF-8
+# export LANGUAGE=en_US.UTF-8
+
 # https://github.com/asdf-vm/asdf/tags
-ASDF=$ASDFVERSION
+export ASDF=${ASDFVERSION}
 # https://github.com/erlang/otp/tags           
-ERLANG=$ERLANGVERSION
+export ERLANG=${ERLANGVERSION}
 # https://github.com/elixir-lang/elixir/tags
 # Compatibility between Erlang and Elixir versions:
 # https://hexdocs.pm/elixir/1.14.4/compatibility-and-deprecations.html
-ELIXIR=$ELIXIRVERSION
+export ELIXIR=${ELIXIRVERSION}
 
 # Update packages
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 # Install ASDF
-ASDFPATH=$HOME/.asdf/bin/asdf
-sudo apt-get install curl git -y
-git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v$ASDF
+export ASDFPATH=${HOME}/.asdf/bin/asdf
+apt-get install curl git -y
+git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf --branch v${ASDF}
 # Ensure ASDF is up to date
-$ASDFPATH update
+${ASDFPATH} update
 # Add ASDF to PATH
-PATH=$HOME/.asdf/shims:$HOME/.asdf/bin:$PATH
+export PATH="${HOME}/.asdf/shims:${HOME}/.asdf/bin:${PATH}"
 
 # Install Erlang & Elixir ASDF plugins
-$ASDFPATH plugin-add erlang
-$ASDFPATH plugin-add elixir
+asdf plugin-add erlang
+${ASDFPATH} plugin-add elixir
 # Ensure ASDF plugins are up to date
-$ASDFPATH plugin-update --all
+${ASDFPATH} plugin-update --all
 # To build Erlang documentation
-KERL_BUILD_DOCS=yes
+export KERL_BUILD_DOCS=yes
 # ASDF Erlang Prereqs
-sudo apt-get -y install build-essential autoconf m4 libncurses5-dev \
+apt-get -y install build-essential autoconf m4 libncurses5-dev \
    libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev \
    libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop \
    libxml2-utils libncurses-dev openjdk-11-jdk
 # Install Erlang from ASDF and set global version
-$ASDFPATH install erlang $ERLANG
-$ASDFPATH global erlang $ERLANG
+${ASDFPATH} install erlang ${ERLANG}
+${ASDFPATH} global erlang ${ERLANG}
 # ASDF Elixir Prereqs
-sudo apt-get install unzip -y
+apt-get install unzip -y
 # Install Elixir from ASDF and set global version
-$ASDFPATH install elixir $ELIXIR
-$ASDFPATH global elixir $ELIXIR
+${ASDFPATH} install elixir ${ELIXIR}
+${ASDFPATH} global elixir ${ELIXIR}
 # Install filesystem watcher for live reloading to work
-sudo apt-get install inotify-tools -y
+apt-get install inotify-tools -y
 # Install Hex Package Manager
-MIXPATH=$HOME/.asdf/installs/elixir/$ELIXIR/bin/mix
-$MIXPATH local.hex --force
+export MIXPATH=${HOME}/.asdf/installs/elixir/${ELIXIR}/bin/mix
+${MIXPATH} local.hex --force
 # Install rebar3 to build Erlang dependencies
-$MIXPATH local.rebar --force
+${MIXPATH} local.rebar --force
 # Install Phoenix Framework Application Generator
-$MIXPATH archive.install hex phx_new --force
+${MIXPATH} archive.install hex phx_new --force
 
+echo $PATH
 echo 'Done!'
---
+++ test/elixir-asdf/version-specific.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# check "asdf" ${HOME}/.asdf/bin/asdf info
+# check "erlang" erl --version
+check "elixir" elixir --version
+
+# Report result
+reportResults
