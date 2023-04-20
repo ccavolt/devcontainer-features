335597370ba3d9f8bf4eeecd42077c7b4f6f44b6
Corbin Cavolt <ccavolt@pm.me>
2023-04-20 20:16:10 +0000

    Troubleshooting checks

--- test/elixir-asdf/version-specific.sh
+++ test/elixir-asdf/version-specific.sh
@@ -8,7 +8,11 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # check "asdf" ${HOME}/.asdf/bin/asdf info
 # check "erlang" erl --version
-check "elixir" bash -c "${HOME}/.asdf/bin/asdf --version"
+check "elixir" ${HOME}/.asdf/bin/asdf --version
+check "bashrc" cat ~/.bashrc
+check "path" echo $PATH
+check "shell" echo "$SHELL"
+check "whoami" which whoami
 
 # Report result
 reportResults
