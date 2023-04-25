fccdec7ce5a0c2efe2790c60c6ce3921296575db
Corbin Cavolt <ccavolt@pm.me>
2023-04-25 00:37:37 +0000

    Add variables add logic to enable less than latest install

--- src/pgadmin4/install.sh
+++ src/pgadmin4/install.sh
@@ -9,8 +9,9 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
-export PGADMIN_SETUP_EMAIL='ccavolt@pm.me'
-export PGADMIN_SETUP_PASSWORD='asdfasdf'
+PGADMIN_VERSION="${VERSION:-"latest"}"
+export PGADMIN_SETUP_EMAIL="${PGADMINSETUPEMAIL:-"john@smith.dev"}"
+export PGADMIN_SETUP_PASSWORD="${PGADMINSETUPPASSWORD:-"asdfasdf"}"
 
 # Install pgAdmin 4
 apt-get update && apt-get upgrade -y
@@ -21,19 +22,18 @@ curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | gpg --dearmo
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt-get update'
 # Install pgAdmin
 # Have to specify installing server version (not just web) so install won't fail
-# apt-get install pgadmin4-server=${PGADMIN4} pgadmin4-web=${PGADMIN4} -y
-apt-get install -y pgadmin4-server pgadmin4-web
+if [ "$PGADMIN_VERSION" != "latest" ]
+then
+    apt-get install -y pgadmin4-server="${PGADMIN_VERSION}" pgadmin4-web="${PGADMIN_VERSION}"
+else
+    apt-get install -y pgadmin4-server pgadmin4-web
+fi
 # Add local python binaries to path
 export PATH=$PATH:${HOME}/.local/bin
 # # Install venv
 apt-get install -y python3-venv
 # Add pgAdmin config
 cp config_local.py /usr/pgadmin4/web
-cat /usr/pgadmin4/web/config_local.py
-echo "email address"
-echo $PGADMIN_SETUP_EMAIL
-echo "pgadmin password"
-echo $PGADMIN_SETUP_PASSWORD
 # Setup pgAdmin web server
 /usr/pgadmin4/bin/setup-web.sh --yes
 # Add local postgres server config to pgAdmin
