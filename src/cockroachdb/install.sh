df3dc05b3c02aa74c04c89a0c335ebf63028a0f2
Corbin Cavolt <ccavolt@pm.me>
2023-05-06 01:31:11 -0600

    Initial attempt to add CockroachDB as feature

---
+++ src/cockroachdb/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -i
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+# https://github.com/cockroachdb/cockroach/tags
+# CockroachDB version
+export CRDB_VERSION=${VERSION:-"22.2.8"}
+
+# Create download directory
+export DOWNLOADDIR=$HOME/downloads
+mkdir $DOWNLOADDIR
+
+# Update packages
+apt-get update && apt-get upgrade -y
+
+# Install CockroachDB dependencies
+apt-get install -y curl libc6 libncurses6 tzdata
+# Download CockroachDB and unzip
+cd $DOWNLOADDIR
+curl "https://binaries.cockroachdb.com/cockroach-v${CRDB_VERSION}.linux-amd64.tgz" | tar -xzv
+# Move CockroachDB Binary
+mv cockroach-v${CRDB_VERSION}.linux-amd64/cockroach /usr/local/bin/
+# Create directory and move lib
+mkdir -p /usr/local/lib/cockroach
+mv cockroach-v${CRDB_VERSION}.linux-amd64/lib/* /usr/local/lib/cockroach/
+
+echo 'Done!'
---
+++ test/cockroachdb/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Start CockroachDB
+cockroach start-single-node \
+  --http-addr=localhost:9999 \
+  --insecure \
+  --accept-sql-without-tls \
+  --background \
+  --log-config-file=./db/crdb-log-config.yaml
+
+# Feature-specific tests
+check "CockroachDB Version" cockroach version
+
+# Report result
+reportResults
