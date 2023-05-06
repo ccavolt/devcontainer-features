ea700e3720ac92b85761eea912f48716992b7d44
Corbin Cavolt <ccavolt@pm.me>
2023-05-06 07:43:39 +0000

    Don't need to start cockroach to get version

--- test/cockroachdb/test.sh
+++ test/cockroachdb/test.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -euxo pipefail
 
 # Optional: Import test library bundled with the devcontainer CLI
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
-
-# Start CockroachDB
-cockroach start-single-node \
-  --http-addr=localhost:9999 \
-  --insecure \
-  --accept-sql-without-tls \
-  --background \
-  --log-config-file=./db/crdb-log-config.yaml
 
 # Feature-specific tests
 check "CockroachDB Version" cockroach version
