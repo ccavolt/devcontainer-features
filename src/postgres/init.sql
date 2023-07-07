4a0ae621c8398f1615126714b2699faa161699ef
Corbin Cavolt <ccavolt@pm.me>
2023-07-07 12:23:55 -0400

    Create database with name of user so connection that doesn't specify a database doesn't fail

--- src/postgres/init.sql
+++ src/postgres/init.sql
@@ -1,3 +1,6 @@
 -- SELECT :'pguser';
 -- SELECT :'pgpass';
-ALTER USER :'pguser' PASSWORD :'pgpass';
+-- Set the password for the user
+alter user :'pguser' password :'pgpass';
+-- Create database with name of user so connection that doesn't specify a database doesn't fail
+create database :'pguser';
