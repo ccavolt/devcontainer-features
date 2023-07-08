488d75c2a300c434be649a59a406c0c03f493bdb
Corbin Cavolt <ccavolt@pm.me>
2023-07-08 02:18:05 +0000

    Add bat as feature

---
+++ src/bat/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Ensure script is running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Prevent installers from trying to prompt for information
+export DEBIAN_FRONTEND=noninteractive
+
+# Update packages
+apt-get update && apt-get upgrade -y
+
+# Install bat
+apt-get install -y bat
+
+echo 'Done!'
