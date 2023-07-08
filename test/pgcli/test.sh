8ce4d5f5f66e986bc7055bf89216da6a71c9818c
Corbin Cavolt <ccavolt@pm.me>
2023-07-08 01:53:42 +0000

    Fix failing test

--- test/pgcli/test.sh
+++ test/pgcli/test.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 source dev-container-features-test-lib
 
 # check where pgcli is installed
-check "where is pgcli" pip3 show pgcli | grep /root/.local/lib
+check "where is pgcli" pip3 show pgcli | grep /usr/local/lib
 # Check that pgcli can execute
 check "pgcli version" pgcli --version
 
