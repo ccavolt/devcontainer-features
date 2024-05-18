b4a5a78880b9c7e4a2503c594cba78be6a4e42ad
Corbin Cavolt <ccavolt@pm.me>
2024-05-18 19:43:30 +0000

    Fix test formatting

--- test/_global/vfox-elixir-version-specific.sh
+++ test/_global/vfox-elixir-version-specific.sh
@@ -12,9 +12,9 @@ check "locale" locale | grep en_US.UTF-8
 check "hex" mix hex.info
 check "vfox elixir script" cat /etc/profile.d/vfox-elixir.sh | grep LANG
 check "check for toolversions" cat /home/vscode/.version-fox/.tool-versions
-check for "hex" ls -la /home/vscode/.mix/archives | grep hex
-check for "phx_new" ls -la /home/vscode/.mix/archives | grep phx_new
-check for "rebar" ls -la /home/vscode/.mix/elixir/1-16 | grep rebar3
+check "check for hex" ls -la /home/vscode/.mix/archives | grep hex
+check "check for phx_new" ls -la /home/vscode/.mix/archives | grep phx_new
+check "check for rebar" ls -la /home/vscode/.mix/elixir/1-16 | grep rebar3
 
 # Report result
 reportResults
