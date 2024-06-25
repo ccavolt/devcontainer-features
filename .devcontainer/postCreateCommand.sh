5a657de01baa6c6222779f89415f832b75ca14be
Corbin Cavolt <ccavolt@pm.me>
2024-06-25 21:13:26 +0000

    Update stuff

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,7 +10,7 @@ npm install -g npm@latest
 
 # Install devcontainer cli
 # https://github.com/devcontainers/cli/tags
-npm install -g @devcontainers/cli@0.59.1
+npm install -g @devcontainers/cli@0.65.0
 
 # Remove explainshell after rebuild to prevent errors
 docker rm --volumes --force "$(docker ps --all --quiet --filter ancestor=spaceinvaderone/explainshell)" || true
