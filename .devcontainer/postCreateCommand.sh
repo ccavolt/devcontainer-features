c6eb220c38a2ffeb418abbf2577972f535c6e00a
Corbin Cavolt <ccavolt@pm.me>
2024-06-25 21:09:45 +0000

    Make postCreateCommand idempotent

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,5 +12,7 @@ npm install -g npm@latest
 # https://github.com/devcontainers/cli/tags
 npm install -g @devcontainers/cli@0.59.1
 
-# this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscode is installed
+# Remove explainshell after rebuild to prevent errors
+docker rm --volumes --force "$(docker ps --all --quiet --filter ancestor=spaceinvaderone/explainshell)" || true
+# This will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscode is installed
 docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
