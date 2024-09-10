0a34286d89bfa5262b2e542fe622bb98366f37fb
Corbin Cavolt <ccavolt@pm.me>
2024-09-10 23:23:19 +0000

    Add flutter feature

---
+++ test/_global/vfox-flutter-test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Optional: Import test library bundled with the devcontainer CLI
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "Flutter version" flutter --version
+
+# Report result
+reportResults
