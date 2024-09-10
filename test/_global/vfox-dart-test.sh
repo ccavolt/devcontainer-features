fd8b7b65446d29ea37a30881eda5801af529d7f3
Corbin Cavolt <ccavolt@pm.me>
2024-09-10 20:47:37 +0000

    Add dart feature

---
+++ test/_global/vfox-dart-test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Optional: Import test library bundled with the devcontainer CLI
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "Dart version" dart --version
+
+# Report result
+reportResults
